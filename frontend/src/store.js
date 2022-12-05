a07eabfa350d1a649a354f0557200b5c2986dd13
tushar453030 <tushar4530@gmail.com>
2022-12-05 22:38:29 +0530

    First commit

---
+++ frontend/src/store.js
@@ -0,0 +1,41 @@
+import { combineReducers, applyMiddleware } from 'redux'
+import { configureStore } from "@reduxjs/toolkit";
+import thunk from 'redux-thunk'
+import { composeWithDevTools} from 'redux-devtools-extension'
+import { productListReducer, productDetailsReducer } from './reducers/productReducers'; 
+import { cartReducer } from './reducers/cartReducers';
+import Loader from './components/Loader';
+import { userLoginReducer,userRegisterReducer,userDetailsReducer, userUpdateProfileReducer } from './reducers/userReducers';
+
+
+const reducer= combineReducers({
+    productList: productListReducer,
+    productDetails: productDetailsReducer,
+    cart: cartReducer,
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
+    userDetails: userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer,
+})
+
+const cartItemsFromStorage=localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+
+const userInfoFromStorage=localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+
+const initialState= {
+    cart: { cartItems: cartItemsFromStorage },
+    userLogin: {userInfo:userInfoFromStorage},
+}
+
+const middleware= [thunk]
+
+const store = configureStore(
+    {reducer: reducer}, 
+    initialState, 
+    composeWithDevTools(
+        applyMiddleware(...middleware)
+    )
+)
+
+
+export default store
